892d4a8ff03b9cbf5d1ffd70cb5552313b33faee
treyweldon <trey@millcreeklimited.com>
2024-03-17 12:44:02 -0400

    part 5

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,54 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+
+MEALS = (
+   ('M', 'Morning'),
+   ('E', 'Evening')
+)
 
 # Create your models here.
+class Accessory(models.Model):
+  name = models.CharField(max_length=50)
+  material = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('acc_detail', kwargs={'pk': self.id})
+
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
+    accessory = models.ManyToManyField(Accessory)
 
     def __str__(self):
         return f'{self.name}({self.id})'
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
+
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+    
+class Feeding(models.Model):
+  date = models.DateField('Feeding Date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+  # Create a cat_id FK
+  finch = models.ForeignKey(
+    Finch,
+    on_delete=models.CASCADE
+  )
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+
+  class Meta:
+    ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-
-from .models import Finch
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Finch, Accessory
+from .forms import FeedingForm
 
 # finches = [
 #   {'name': 'Goldie', 'breed': 'Goldfinch', 'description': 'exuberant bounding flight, musical calls, and flashy yellow and black plumage'},
@@ -23,4 +25,60 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
-  return render(request, 'finches/detail.html', { 'finch': finch })
+  id_list = finch.accessory.all().values_list('id')
+  acc_finch_doesnt_have = Accessory.objects.exclude(id__in=id_list)
+  feeding_form = FeedingForm()
+  return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form, 'accessories': acc_finch_doesnt_have })
+
+# 
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = ['name', 'breed', 'description']
+
+  def form_valid(self, form):
+    return super().form_valid(form)
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['breed', 'description']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finch'
+
+
+def add_feeding(request, finch_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
+
+
+class AccList(ListView):
+  model = Accessory
+
+class AccDetail(DetailView):
+  model = Accessory
+
+class AccCreate(CreateView):
+  model = Accessory
+  fields = '__all__'
+
+class AccUpdate(UpdateView):
+  model = Accessory
+  fields = ['name', 'material']
+
+class AccDelete(DeleteView):
+  model = Accessory
+  success_url = '/accessories'
+
+def assoc_acc(request, finch_id, acc_id):
+  Finch.objects.get(id=finch_id).accessory.add(acc_id)
+  return redirect('detail', finch_id=finch_id)
+
+def unassoc_acc(request, finch_id, acc_id):
+  Finch.objects.get(id=finch_id).accessory.remove(acc_id)
+  return redirect('detail', finch_id=finch_id)
